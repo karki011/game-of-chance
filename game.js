396d16972ce6eb7b41eccc96012bf09389215659
Binup karki <karki011@gmail.com>
2019-09-09 23:54:37 -0500

    added eventlisner

--- game.js
+++ game.js
@@ -9,3 +9,13 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+
+rock_div.addEventListener('click', function(){
+    console.log("You clicke rock!"); 
+});
+paper_div.addEventListener('click', function(){
+    console.log("You clicke paper!"); 
+});
+scissors_div.addEventListener('click', function(){
+    console.log("You clicke scissors!"); 
+});
