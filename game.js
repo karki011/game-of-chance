297912510875b19a617ba0be2d0ebc8fe83489d7
Binup karki <karki011@gmail.com>
2019-09-10 00:50:54 -0500

    added win() lose() draw()

--- game.js
+++ game.js
@@ -1,35 +1,69 @@
 //need score for both user and computer
 //we are storing all the dom in variable so easy uses
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let  computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+const smUserWord = "user".fontsize(3).sub();
+const smComputerWord = "comp".fontsize(3).sub();
 
 const getComputerChoice = function () {
-    const choices = ["r", "p", "s"];
+    const choices = ["Rock", "Paper", "Scissors"];
     const randonNumber = Math.floor(Math.random() * 3); //gets random number between 0 -3
     return choices[randonNumber];
 }
-getComputerChoice();
-console.log('getComputerChoice():', getComputerChoice())
-const game = function (userChoice) {
 
+const win = function(user, computer){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${user}${smUserWord} beats ${computer}${smComputerWord}. You Win!`
 }
+const lose = function(user, computer){
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${user}${smUserWord} loses to ${computer}${smComputerWord}. You lost!`  
+}
+const draw = function(user, computer){
+    result_p.innerHTML = `${user}${smUserWord} draws with ${computer}${smComputerWord}. It's a draw!`    
+}
+const game = function (userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case 'RockScissors':
+        case 'PaperRock':
+        case 'ScissorsPaper':
+            win(userChoice, computerChoice );
+            break;
+        case 'RockPaper':
+        case 'PaperScissors':
+        case 'ScissorsRock':
+            lose(userChoice, computerChoice);
+            break;
+        case 'RockRock':
+        case 'PaperPaper':
+        case 'ScissorsScissors':
+            draw(userChoice, computerChoice);
+            break;
+    }
+}
+
 const main = function () {
     rock_div.addEventListener('click', function () {
-        game("r");
+        game("Rock");
     });
     paper_div.addEventListener('click', function () {
-        game("p");
+        game("Paper");
 
     });
     scissors_div.addEventListener('click', function () {
-        game("s");
+        game("Scissors");
     });
 }
 
